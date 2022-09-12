9581e4bc21f53a28a1c460388145a8083eac242e
Will Humphlett <will@humphlett.net>
2022-09-12 11:45:31 -0500

    Add workshop 2

---
+++ W2-Taint-Tracking/README.md
@@ -0,0 +1,6 @@
+# W2-Taint-Tracking
+Using abstract syntax trees to track variables as they move through code  
+*full workshop outline can be found in Workshop2.md*
+
+## Commands Run
+1. `python workshop2-anaylsis.py`
---
+++ W2-Taint-Tracking/workshop2-calc.py
@@ -0,0 +1,20 @@
+def simpleCalculator(v1, v2, operation):
+    res = 0 
+    if operation=='+':
+        res = v1 + v2 
+    elif operation=='-':
+        res = v1 - v2 
+    elif operation=='*':
+        res = v1 * v2 
+    elif operation=='/':                
+        res = v1 / v2 
+    elif operation=='%':                
+        res = v1 % v2 
+    return res 
+
+
+if __name__=='__main__':
+    val1, val2, op = 100, 1, '+'
+
+    data = simpleCalculator(val1, val2, op)
+    print('Operation:{}\nResult:{}'.format(  op, data  ) )
